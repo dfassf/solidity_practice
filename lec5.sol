72c42a5a884b45600a356dd6c5c143e4ef0cdd94
Shinwoo Um <dfassf@gmail.com>
2022-03-16 16:59:14 +0900

    lec6

--- lec5.sol
+++ lec5.sol
@@ -1,48 +1,42 @@
 // SPDX-License-Identifier: GPL-30
 pragma solidity >=0.7.0 <0.9.0;
 
-contract lec5 {
+contract lec6 {
     /*
         function 이름 () public{ // (public, private, internal, external)
             // 내용
         }
     */
 
-    /*
-    public: 모든 곳에서 접근 가능
-    external: public 처럼 모든 곳에서 접근 가능하나, external이 정의된 자기 자신의 contract에서는 접근 불가
-    private: private이 정의된 자기 contract에서만 접근 가능(private이 정의된 contract를 상속받은 자식도 불가능)
-    internal: private처럼 오직 internal이 정의된 자기 자신의 contract에서만 가능하고, internal이 정의된 contract를 상속받은 자식도 불가능
+    /* 
+    function get_a() external view returns(uint256)
     */
 
-    // 1. public
-    uint256 public a = 5;
+    /*
+    view: function 밖의 변수를 읽을 수 있으나 변경 불가능
+    pure: function 밖의 변수들을 읽을 수 없고 변경도 불가능
+    view와 pure 둘 다 명시 안할 때: function 밖의 변수들을 읽어서 변경 해야 함
+    */
 
-    // 2. private
-    uint256 public a2 = 5;
-}
+    // 1. view
+    uint256 public a = 1;
 
-contract public_example {
-    uint256 public a = 3;
-
-    function changeA(uint256 _value) external {
-        a = _value;
+    function read_a() public view returns (uint256) {
+        return a + 2;
+        // 안바뀜
     }
 
-    function get_a() public view returns (uint256) {
+    function read_b() public pure returns (uint256) {
+        uint256 b = 1;
+        return 4 + 2 + b;
+        // 안의 것만.
+        // a를 쓰면 typeError가 뜸
+    }
+
+    function read_c() public returns (uint256) {
+        a = 13;
+        // a를 바꾸기 때문에 pure, view 불가능
+        // 리턴값 13이 나옴
         return a;
     }
-}
-
-contract public_example_2 {
-    // contract를 쓰기 위해 instance화 시켜야 함
-    public_example instance = new public_example();
-
-    function changeA_2(uint256 _value) public {
-        instance.changeA(_value);
-    }
-
-    function use_public_example_a() public view returns (uint256) {
-        return instance.get_a();
-    } // public_example의 changeA(), get_a()를 private로 바꾸면 작동 X
 }
