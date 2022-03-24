12aaaa717a62c2f5cc5ec4f4af7ef97baf3ee789
Shinwoo Um <dfassf@gmail.com>
2022-03-24 14:02:00 +0900

    lec15

---
+++ lec15.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-30
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Father {
+    event FatherName(string name);
+    function who() public virtual{
+        emit FatherName("KimDaeHo");
+    }
+}
+
+contract Son is Father{
+    event sonName(string name);
+    function who() public override{
+        super.who();
+        emit sonName("Kimjin");
+    }
+}
