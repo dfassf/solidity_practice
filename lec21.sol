d4e74438236ae3847e08e3158e5112f3e9c25bee
Shinwoo Um <dfassf@gmail.com>
2022-04-17 23:10:57 +0900

    lec21

---
+++ lec21.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-30
+pragma solidity >=0.7.0 <0.9.0;
+
+contract lec21 {
+/* 
+    case 1: if-else
+    if(if가 발동되는 조건){
+        if 내용
+    }
+    else{if가 발동이 안되면 
+        else 내용
+    }
+
+    case 2: if-else if-else
+     if(if가 발동되는 조건){
+        if 내용
+    }
+    else if(else if가 발동되는 조건){if가 발동이 안되면 
+        else 내용
+    }
+    ....
+    else{if, else if가 발동이 안되면
+        else 내용
+    }
+*/
+
+    string private outcome = "";
+    function isIt5(uint256 _number) public returns(string memory) {
+        if(_number == 5) {
+            outcome = "yes it is 5";
+            return outcome;
+        }
+        else { // 이 경우에는 else가 없어도 작동함 
+            outcome = "nope";
+            return outcome;
+        }
+    }
+}
