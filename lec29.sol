3fbb375189f65e2d75a1a7f2ea111533645e8904
Shinwoo Um <dfassf@gmail.com>
2022-05-11 00:36:32 +0900

    lec29

--- lec29.sol
+++ lec29.sol
@@ -9,6 +9,7 @@ contract lec29 {
 
     function add2(uint256 _num1, uint256 _num2) public pure returns (uint256 total) {
         total = _num1 + _num2;
+        // 매개변수가 많아지면 헷갈릴 수 있음,,
         return total;
     }
 }
